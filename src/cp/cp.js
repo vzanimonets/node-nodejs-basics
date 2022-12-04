60422fd4f09e2ce37cf85ba1cd1aa0d034f1b0c4
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-04 12:24:33 +0300

    feat: implementation of functions for Child Process(src/cp)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,21 @@
+import {fork} from 'node:child_process'
+import {fileURLToPath} from 'url'
+import path from 'path'
+
+const filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(filename);
+const filepath = path.join(__dirname,'files','script.js')
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork( filepath, [...args.split(' ')], { silent: true })
+
+    process.stdin.on('data', (data) => {
+        childProcess.stdin.write(data)
+    })
+
+    childProcess.stdout.on('data', (data) => {
+        console.log(data.toString())
+    });
 };
 
-spawnChildProcess();
+spawnChildProcess('--version');
